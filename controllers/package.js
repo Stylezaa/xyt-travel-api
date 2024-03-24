038595c3226aa3ecd50b9c2c4da4451da1337c1c
DevSin <48406477+Stylezaa@users.noreply.github.com>
2024-03-24 22:58:06 +0700

    add .

--- controllers/package.js
+++ controllers/package.js
@@ -1,98 +1,98 @@
-const Package = require("../models/package");
+const Package = require('../models/package')
 
 exports.GetPackageAll = async (req, res) => {
   try {
     let package = await Package.find()
-      .populate({ path: "users_booking" })
-      .populate({ path: "category" })
-      .sort({ createdAt: -1 });
+      .populate({ path: 'users_booking' })
+      .populate({ path: 'category' })
+      .sort({ createdAt: -1 })
 
-    const basePath = `${req.protocol}://${req.get("host")}/uploads/packages/`;
+    const basePath = `${req.protocol}://${req.get('host')}/uploads/packages/`
 
     if (package.length === 0) {
       return res.status(404).send({
-        message: "Not Found Any Package",
+        message: 'Not Found Any Package',
         status: 404,
-      });
+      })
     }
 
     res.status(200).send({
       message: package,
       url: basePath,
       status: 200,
-    });
+    })
   } catch (error) {
     res.status(500).send({
       message: error,
       status: 500,
-    });
+    })
   }
-};
+}
 
 exports.GetPackageByCate = async (req, res) => {
   try {
-    const { category } = req.query;
+    const { category } = req.query
     let package = await Package.find()
       .populate({
-        path: "users_booking",
+        path: 'users_booking',
       })
       .populate({
-        path: "category",
+        path: 'category',
         match: {
           cate_name: category,
         },
       })
       .sort({ createdAt: -1 })
       .then((data) => {
-        return data.filter((parent) => parent.category !== null);
-      });
+        return data.filter((parent) => parent.category !== null)
+      })
 
-    const basePath = `${req.protocol}://${req.get("host")}/uploads/packages/`;
+    const basePath = `${req.protocol}://${req.get('host')}/uploads/packages/`
 
     if (package.length === 0) {
       return res.status(404).send({
-        message: "Not Found Any Package",
+        message: 'Not Found Any Package',
         status: 404,
-      });
+      })
     }
 
     res.status(200).send({
       message: package,
       url: basePath,
       status: 200,
-    });
+    })
   } catch (error) {
     res.status(500).send({
       message: error,
       status: 500,
-    });
+    })
   }
-};
+}
 
 exports.GetPackageByID = async (req, res) => {
   try {
-    const { title } = req.params;
-    let packageOne = await Package.findOne({ title: title })
-      .populate("users_booking")
-      .populate({ path: "category" })
-      .exec();
+    const { id } = req.params
+    let packageOne = await Package.findOne({ _id: id })
+      .populate('users_booking')
+      .populate({ path: 'category' })
+      .exec()
     if (!packageOne) {
       return res.status(404).send({
-        message: "Not Found This Package",
+        message: 'Not Found This Package',
         status: 404,
-      });
+      })
     }
     res.status(200).send({
       message: packageOne,
       status: 200,
-    });
+    })
   } catch (err) {
     res.status(500).send({
       message: err,
       status: 500,
-    });
+    })
   }
-};
+}
 
 exports.Insert = async (req, res) => {
   try {
@@ -118,156 +118,158 @@ exports.Insert = async (req, res) => {
       meals_pocket,
       booking_policy,
       enabled,
-    } = req.body;
+    } = req.body
 
-    const files1 = req.files["cover"][0];
-    const files2 = req.files["cover_itinerary"];
+    console.log({ activities_pocket })
 
-    if (!files1 || files2.length === 0) {
-      return res.status(404).send({
-        message: "ໄຟຮຮູບພາບບໍ່ຄົບຖ້ວນ",
-        status: 404,
-      });
-    }
+    // const files1 = req.files['cover'][0]
+    // const files2 = req.files['cover_itinerary']
 
-    let ItineraryArray = [];
-    for (let index = 0; index < files2.length; index++) {
-      ItineraryArray.push({
-        title_itinerary: title_itinerary[index],
-        cover_itinerary: files2[index].filename,
-        content_itinerary: content_itinerary[index],
-      });
-    }
+    // if (!files1 || files2.length === 0) {
+    //   return res.status(404).send({
+    //     message: 'ໄຟຮຮູບພາບບໍ່ຄົບຖ້ວນ',
+    //     status: 404,
+    //   })
+    // }
 
-    let PocketArray = [];
-    for (let index = 0; index < title_pocket.length; index++) {
-      PocketArray.push({
-        title_pocket: title_pocket[index],
-        activities_pocket: activities_pocket[index],
-        where_to_stay_pocket: where_to_stay_pocket[index],
-        meals_pocket: meals_pocket[index],
-      });
-    }
+    // let ItineraryArray = []
+    // for (let index = 0; index < files2.length; index++) {
+    //   ItineraryArray.push({
+    //     title_itinerary: title_itinerary[index],
+    //     cover_itinerary: files2[index].filename,
+    //     content_itinerary: content_itinerary[index],
+    //   })
+    // }
 
-    let package = new Package({
-      title,
-      price,
-      category,
-      duration_tour,
-      start_tour,
-      end_tour,
-      cities_tour,
-      min_tour,
-      max_tour,
-      trip_overview,
-      booking_policy,
-      enabled,
-    });
+    // let PocketArray = []
+    // for (let index = 0; index < title_pocket.length; index++) {
+    //   PocketArray.push({
+    //     title_pocket: title_pocket[index],
+    //     activities_pocket: activities_pocket[index],
+    //     where_to_stay_pocket: where_to_stay_pocket[index],
+    //     meals_pocket: meals_pocket[index],
+    //   })
+    // }
 
-    await package.save(); // Save to database
+    // let package = new Package({
+    //   title,
+    //   price,
+    //   category,
+    //   duration_tour,
+    //   start_tour,
+    //   end_tour,
+    //   cities_tour,
+    //   min_tour,
+    //   max_tour,
+    //   trip_overview,
+    //   booking_policy,
+    //   enabled,
+    // })
 
-    if (enabled) {
-      package.enabled = req.body.enabled;
-    }
+    // await package.save() // Save to database
 
-    if (recommend_for) {
-      await Package.updateOne(
-        {
-          _id: package._id,
-        },
-        { $push: { recommend_for: recommend_for } }
-      );
-    }
+    // if (enabled) {
+    //   package.enabled = req.body.enabled
+    // }
 
-    if (files1) {
-      await Package.updateOne(
-        {
-          _id: package._id,
-        },
-        {
-          cover: files1.filename,
-        }
-      );
-    }
+    // if (recommend_for) {
+    //   await Package.updateOne(
+    //     {
+    //       _id: package._id,
+    //     },
+    //     { $push: { recommend_for: recommend_for } }
+    //   )
+    // }
 
-    if (Array.isArray(title_itinerary)) {
-      for (let index = 0; index < title_itinerary.length; index++) {
-        await Package.updateOne(
-          {
-            _id: package._id,
-          },
-          {
-            itinerary: ItineraryArray,
-          }
-        );
-      }
-    } else {
-      let itineraryItem = {
-        title_itinerary: title_itinerary,
-        content_itinerary: content_itinerary,
-        cover_itinerary: files2[0].filename,
-      };
-      await Package.updateOne(
-        {
-          _id: package._id,
-        },
-        {
-          itinerary: itineraryItem,
-        }
-      );
-    }
+    // if (files1) {
+    //   await Package.updateOne(
+    //     {
+    //       _id: package._id,
+    //     },
+    //     {
+    //       cover: files1.filename,
+    //     }
+    //   )
+    // }
 
-    if (Array.isArray(title_pocket)) {
-      for (let index = 0; index < title_pocket.length; index++) {
-        await Package.updateOne(
-          {
-            _id: package._id,
-          },
-          {
-            pocket_summary: PocketArray,
-          }
-        );
-      }
-    } else {
-      let PocketItem = {
-        title_pocket,
-        activities_pocket,
-        where_to_stay_pocket,
-        meals_pocket,
-      };
-      await Package.updateOne(
-        {
-          _id: package._id,
-        },
-        {
-          pocket_summary: PocketItem,
-        }
-      );
-    }
+    // if (Array.isArray(title_itinerary)) {
+    //   for (let index = 0; index < title_itinerary.length; index++) {
+    //     await Package.updateOne(
+    //       {
+    //         _id: package._id,
+    //       },
+    //       {
+    //         itinerary: ItineraryArray,
+    //       }
+    //     )
+    //   }
+    // } else {
+    //   let itineraryItem = {
+    //     title_itinerary: title_itinerary,
+    //     content_itinerary: content_itinerary,
+    //     cover_itinerary: files2[0].filename,
+    //   }
+    //   await Package.updateOne(
+    //     {
+    //       _id: package._id,
+    //     },
+    //     {
+    //       itinerary: itineraryItem,
+    //     }
+    //   )
+    // }
 
-    if (!package) {
-      return res.status(404).send({
-        message: "Not Found Any Data",
-        status: 404,
-      });
-    }
+    // if (Array.isArray(title_pocket)) {
+    //   for (let index = 0; index < title_pocket.length; index++) {
+    //     await Package.updateOne(
+    //       {
+    //         _id: package._id,
+    //       },
+    //       {
+    //         pocket_summary: PocketArray,
+    //       }
+    //     )
+    //   }
+    // } else {
+    //   let PocketItem = {
+    //     title_pocket,
+    //     activities_pocket,
+    //     where_to_stay_pocket,
+    //     meals_pocket,
+    //   }
+    //   await Package.updateOne(
+    //     {
+    //       _id: package._id,
+    //     },
+    //     {
+    //       pocket_summary: PocketItem,
+    //     }
+    //   )
+    // }
 
-    res.status(201).send({
-      message: "Insert Product Successfully",
-      status: 201,
-    });
+    // if (!package) {
+    //   return res.status(404).send({
+    //     message: 'Not Found Any Data',
+    //     status: 404,
+    //   })
+    // }
+
+    // res.status(201).send({
+    //   message: 'Insert Product Successfully',
+    //   status: 201,
+    // })
   } catch (error) {
-    console.log(error);
+    console.log(error)
     res.status(500).send({
       message: error,
       status: 500,
-    });
+    })
   }
-};
+}
 
 exports.UpdatePackage = async (req, res, next) => {
   try {
-    const { id } = req.params;
+    const { id } = req.params
 
     const {
       title,
@@ -296,18 +298,18 @@ exports.UpdatePackage = async (req, res, next) => {
 
       booking_policy,
       enabled,
-    } = req.body;
+    } = req.body
 
-    const files1 = (req.files["cover"] && req.files["cover"][0]) || null;
-    const files2 = req.files["cover_itinerary"] || null;
+    const files1 = (req.files['cover'] && req.files['cover'][0]) || null
+    const files2 = req.files['cover_itinerary'] || null
 
     // Check ID has found ?
-    const ExistPackage = await Package.findById(id);
+    const ExistPackage = await Package.findById(id)
     if (!ExistPackage) {
       return res.status(404).send({
-        message: "ບໍ່ພົບ Package ນີ້",
+        message: 'ບໍ່ພົບ Package ນີ້',
         status: 404,
-      });
+      })
     }
 
     await Package.updateOne(
@@ -328,7 +330,7 @@ exports.UpdatePackage = async (req, res, next) => {
         booking_policy,
         enabled,
       }
-    );
+    )
 
     if (files1) {
       await Package.updateOne(
@@ -338,7 +340,7 @@ exports.UpdatePackage = async (req, res, next) => {
         {
           cover: files1.filename,
         }
-      );
+      )
     }
 
     if (recommend_for) {
@@ -347,25 +349,25 @@ exports.UpdatePackage = async (req, res, next) => {
           _id: id,
         },
         { $push: { recommend_for: recommend_for } }
-      );
+      )
     }
 
     if (files2?.length > 0) {
-      if (typeof cover_itinerary === "object") {
-        let ItineraryArray = [];
+      if (typeof cover_itinerary === 'object') {
+        let ItineraryArray = []
         for (let index = 0; index < files2?.length; index++) {
           cover_itinerary.splice(
             parseInt(cover_itinerary_index[index]),
             0,
             files2[index]?.filename
-          );
+          )
         }
         for (let index = 0; index < title_itinerary.length; index++) {
           ItineraryArray.push({
             title_itinerary: title_itinerary[index],
             cover_itinerary: cover_itinerary[index],
             content_itinerary: content_itinerary[index],
-          });
+          })
         }
 
         await Package.updateOne(
@@ -375,20 +377,20 @@ exports.UpdatePackage = async (req, res, next) => {
           {
             itinerary: ItineraryArray,
           }
-        );
-      } else if (typeof cover_itinerary === "string") {
-        let ItineraryImage = [];
-        ItineraryImage.splice(0, 0, cover_itinerary);
+        )
+      } else if (typeof cover_itinerary === 'string') {
+        let ItineraryImage = []
+        ItineraryImage.splice(0, 0, cover_itinerary)
         for (let index = 0; index < files2.length; index++) {
-          ItineraryImage.push(files2[index].filename);
+          ItineraryImage.push(files2[index].filename)
         }
-        let ItineraryArray = [];
+        let ItineraryArray = []
         for (let index = 0; index < ItineraryImage.length; index++) {
           ItineraryArray.push({
             title_itinerary: title_itinerary[index],
             cover_itinerary: ItineraryImage[index],
             content_itinerary: content_itinerary[index],
-          });
+          })
         }
 
         await Package.updateOne(
@@ -398,16 +400,16 @@ exports.UpdatePackage = async (req, res, next) => {
           {
             itinerary: ItineraryArray,
           }
-        );
+        )
       } else if (!cover_itinerary) {
         if (Array.isArray(title_itinerary)) {
-          let ItineraryArray = [];
+          let ItineraryArray = []
           for (let index = 0; index < title_itinerary.length; index++) {
             ItineraryArray.push({
               title_itinerary: title_itinerary[index],
               cover_itinerary: files2[index]?.filename,
               content_itinerary: content_itinerary[index],
-            });
+            })
           }
 
           await Package.updateOne(
@@ -417,13 +419,13 @@ exports.UpdatePackage = async (req, res, next) => {
             {
               itinerary: ItineraryArray,
             }
-          );
+          )
         } else {
           let ItineraryItem = {
             title_itinerary,
             cover_itinerary: files2[0].filename,
             content_itinerary,
-          };
+          }
           await Package.updateOne(
             {
               _id: id,
@@ -431,18 +433,18 @@ exports.UpdatePackage = async (req, res, next) => {
             {
               itinerary: ItineraryItem,
             }
-          );
+          )
         }
       }
     } else if (!files2) {
       if (Array.isArray(title_itinerary)) {
-        let ItineraryArray = [];
+        let ItineraryArray = []
         for (let index = 0; index < title_itinerary.length; index++) {
           ItineraryArray.push({
             title_itinerary: title_itinerary[index],
             cover_itinerary: cover_itinerary[index],
             content_itinerary: content_itinerary[index],
-          });
+          })
         }
 
         await Package.updateOne(
@@ -452,13 +454,13 @@ exports.UpdatePackage = async (req, res, next) => {
           {
             itinerary: ItineraryArray,
           }
-        );
+        )
       } else {
         let ItineraryItem = {
           title_itinerary,
           cover_itinerary,
           content_itinerary,
-        };
+        }
         await Package.updateOne(
           {
             _id: id,
@@ -466,19 +468,19 @@ exports.UpdatePackage = async (req, res, next) => {
           {
             itinerary: ItineraryItem,
           }
-        );
+        )
       }
     }
 
     // Pocket
-    let PocketArray = [];
+    let PocketArray = []
     for (let index = 0; index < title_pocket.length; index++) {
       PocketArray.push({
         title_pocket: title_pocket[index],
         activities_pocket: activities_pocket[index],
         where_to_stay_pocket: where_to_stay_pocket[index],
         meals_pocket: meals_pocket[index],
-      });
+      })
     }
 
     if (Array.isArray(title_pocket)) {
@@ -490,7 +492,7 @@ exports.UpdatePackage = async (req, res, next) => {
           {
             pocket_summary: PocketArray,
           }
-        );
+        )
       }
     } else {
       let PocketItem = {
@@ -498,7 +500,7 @@ exports.UpdatePackage = async (req, res, next) => {
         activities_pocket,
         where_to_stay_pocket,
         meals_pocket,
-      };
+      }
       await Package.updateOne(
         {
           _id: id,
@@ -506,44 +508,44 @@ exports.UpdatePackage = async (req, res, next) => {
         {
           pocket_summary: PocketItem,
         }
-      );
+      )
     }
 
     res.status(200).json({
-      message: "ອັບເດດ Package ສໍາເລັດ",
+      message: 'ອັບເດດ Package ສໍາເລັດ',
       status: 200,
-    });
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
     res.status(500).send({
       message: error,
       status: 500,
-    });
+    })
   }
-};
+}
 
 exports.DeletePackageById = async (req, res, next) => {
   try {
-    const { id } = req.params;
+    const { id } = req.params
 
-    const dataOne = await Package.deleteOne({ _id: id });
+    const dataOne = await Package.deleteOne({ _id: id })
 
     if (!dataOne) {
       return res.status(404).send({
-        message: "Not Found This Package Item",
+        message: 'Not Found This Package Item',
         status: 404,
-      });
+      })
     }
 
     res.status(200).send({
-      message: "Delete This Package Successfully",
+      message: 'Delete This Package Successfully',
       status: 200,
-    });
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
     res.status(500).send({
       message: error,
       status: 500,
-    });
+    })
   }
-};
+}
--- routes/package.js
+++ routes/package.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // Functions
-const upload = require("../functions/PackageUploadConfig");
+const upload = require('../functions/PackageUploadConfig')
 
 //Controllers
 const {
@@ -12,10 +12,10 @@ const {
   Insert,
   UpdatePackage,
   DeletePackageById,
-} = require("../controllers/package");
+} = require('../controllers/package')
 
 //Middleware
-const { auth, AdminCheck } = require("../middleware/AuthCheck"); // Must has token for access
+const { auth, AdminCheck } = require('../middleware/AuthCheck') // Must has token for access
 
 // ====================================
 // ====== Package Services ========
@@ -24,48 +24,48 @@ const { auth, AdminCheck } = require("../middleware/AuthCheck"); // Must has tok
 //@Endpoint https://localhost:8000/api/package/getall
 //@Method   GET
 //@Access   Publish
-router.get("/package/getall", GetPackageAll);
+router.get('/package/getall', GetPackageAll)
 
 //@Endpoint https://localhost:8000/api/package/getall
 //@Method   GET
 //@Access   Publish
-router.get("/package/getall/category", GetPackageByCate);
+router.get('/package/getall/category', GetPackageByCate)
 
 //@Endpoint https://localhost:8000/api/package/get/:id
-//@Method   POST
+//@Method   GET
 //@Access   Publish
-router.post("/package/get/:id", GetPackageByID);
+router.get('/package/get/:id', GetPackageByID)
 
 //@Endpoint https://localhost:8000/api/package/insert
 //@Method   POST
 //@Access   Private
 router.post(
-  "/package/insert",
+  '/package/insert',
   upload.fields([
-    { name: "cover", maxCount: 1 },
-    { name: "cover_itinerary", maxCount: 20 },
+    { name: 'cover', maxCount: 1 },
+    { name: 'cover_itinerary', maxCount: 20 },
   ]),
   auth,
   Insert
-);
+)
 
 //@Endpoint https://localhost:8000/api/package/update/:id
 //@Method   Patch
 //@Access   Private
 router.patch(
-  "/package/update/:id",
+  '/package/update/:id',
   auth,
   AdminCheck,
   upload.fields([
-    { name: "cover", maxCount: 1 },
-    { name: "cover_itinerary", maxCount: 20 },
+    { name: 'cover', maxCount: 1 },
+    { name: 'cover_itinerary', maxCount: 20 },
   ]),
   UpdatePackage
-);
+)
 
 //@Endpoint https://localhost:8000/api/package/delete/:id
 //@Method   Delete
 //@Access   Private
-router.delete("/package/delete/:id", auth, AdminCheck, DeletePackageById);
+router.delete('/package/delete/:id', auth, AdminCheck, DeletePackageById)
 
-module.exports = router;
+module.exports = router
